4f2b919ed1c9a317a2447d913bffb6fa9bfb3ff5
Marco Antonio Souza <maps@ic.ufal.br>
2020-06-03 18:17:33 -0300

    Cors

--- App.py
+++ App.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, Response
 from flask_httpauth import HTTPBasicAuth
+from flask_cors import CORS
 from web_scraper import unidades, faturas, fatura
 
 app = Flask(__name__)
+CORS(app)
 auth = HTTPBasicAuth()
 
 @app.route("/")
